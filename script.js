b68f5c8bf164d2f1da08c776de645445b780abc2
Claudia118 <claudiaortiz1030@gmail.com>
2023-01-09 22:41:07 -0700

    edit style.css and index.html

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'What did the foreign exchange student take from Michael back to what was formerly Yugoslavia?',
+        a: 'All of his shoes',
+        b: 'All of his blue jeans',
+        c: 'All of his shorts',
+        d: 'All of his toys',
+        correct: 'b'
+    }, {
+        question: 'What does Michael ask Pam to spread on his foot that he burned on a George Foreman grill?',
+        a: 'Mayonnaise',
+        b: 'Mustard',
+        c: 'Butter',
+        d: 'Lotion',
+        correct: 'c'
+    }, {
+        question: 'What does Kevin suggest Dwight put in his gun holster?',
+        a: 'A cell phone',
+        b: 'A banana',
+        c: 'A toy gun',
+        d: 'A twinkie',
+        correct: 'b'
+    }, {
+        question: 'What did Kevin buy for himself when he got himself for Secret Santa?',
+        a: 'A foot bath',
+        b: 'A foot massager',
+        c: 'American Pie 2 movie',
+        d: 'M&Ms',
+        correct: 'b'
+    }, {
+        question: 'Aside from Jim, who in the office has an obvious crush on Pam throughout the show?',
+        a: 'Ryan',
+        b: 'Toby',
+        c: 'Dwight',
+        d: 'Andy',
+        correct: 'b'
+    }
+]
